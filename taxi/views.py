86f60156e597bd1944fb16ef925999dbe70d99d4
Ярослав  Полінкін <yaroslav.ua.polinkin@gmail.com>
2024-11-26 08:54:45 +0200

    solution

--- taxi/views.py
+++ taxi/views.py
@@ -5,6 +5,55 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Driver, Car, Manufacturer
 
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .forms import CarForm, ManufacturerForm
+class CarListView(ListView):
+    model = Car
+    template_name = "car_list.html"
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = "car_detail.html"
+
+class CarCreateView(CreateView):
+    model = Car
+    form_class = CarForm
+    template_name = "car_form.html"
+    success_url = reverse_lazy("car-list")
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarForm
+    template_name = "car_form.html"
+    success_url = reverse_lazy("car-list")
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = "car_confirm_delete.html"
+    success_url = reverse_lazy("car-list")
+
+# Manufacturer views
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    template_name = "manufacturer_list.html"
+
+class ManufacturerCreateView(CreateView):
+    model = Manufacturer
+    form_class = ManufacturerForm
+    template_name = "manufacturer_form.html"
+    success_url = reverse_lazy("manufacturer-list")
+
+class ManufacturerUpdateView(UpdateView):
+    model = Manufacturer
+    form_class = ManufacturerForm
+    template_name = "manufacturer_form.html"
+    success_url = reverse_lazy("manufacturer-list")
+
+class ManufacturerDeleteView(DeleteView):
+    model = Manufacturer
+    template_name = "manufacturer_confirm_delete.html"
+    success_url = reverse_lazy("manufacturer-list")
 
 @login_required
 def index(request):
